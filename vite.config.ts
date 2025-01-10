b5358c9ce847b11836ebcc4f13310f5197762362
Kazem Kamali <itskazemk@gmail.com>
2025-01-10 20:40:23 +0330

    updated dependencies and config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 const projectRoot = new URL("./src", import.meta.url).pathname;
