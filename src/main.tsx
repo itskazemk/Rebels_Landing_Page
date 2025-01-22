b2312b922efecbd4879fb43b12daf86b6879ce9d
Kazem Kamali <itskazemk@gmail.com>
2025-01-22 06:00:57 +0000

    added fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import '@/assets/fonts/fonts.css';
+import { ThemeProvider } from "@/components/theme-provider.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ThemeProvider } from "@/components/theme-provider.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
