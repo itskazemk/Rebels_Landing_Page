ddfeb219764c76ae24c578d5ec9bb186a9f410cf
Kazem <itskazemk@gmail.com>
2025-04-20 13:13:48 +0330

    added daisyUI and fixed Carousel

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -1,169 +1,34 @@
-import { useState } from "react";
-
 export function Carousel() {
-  const [num, setNum] = useState(0);
-
-  function nextFn() {
-    setNum((item) => item + 1);
-  }
-
-  function prevFn() {
-    setNum((item) => item - 1);
-  }
-
   return (
-    <div
-      id="default-carousel"
-      className="relative w-full"
-      data-carousel="slide"
-    >
-      {/* <!-- Carousel wrapper --> */}
-      <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-        {/* <!-- Item 1 --> */}
-        <div
-          className={`${num == 0 ? "" : "hidden "} duration-700 ease-in-out`}
-          data-carousel-item
-        >
-          <img
-            src="images/concert/2-Buy_ticket.jpeg"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
+    <div className="px-8">
+      <div className="carousel w-full">
+        <div id="item1" className="carousel-item w-full">
+          <img src="/images/concert/1-Home_page.jpeg" className="w-full" />
         </div>
-        {/* <!-- Item 2 --> */}
-        <div
-          className={`${num == 1 ? "" : "hidden "} duration-700 ease-in-out`}
-          data-carousel-item
-        >
-          <img
-            src="images/concert/3-Panel.jpeg"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
+        <div id="item2" className="carousel-item w-full">
+          <img src="/images/concert/2-Buy_ticket.jpeg" className="w-full" />
         </div>
-        {/* <!-- Item 3 --> */}
-        <div
-          className={`${num == 0 ? "" : "hidden "} duration-700 ease-in-out`}
-          data-carousel-item
-        >
-          <img
-            src="images/concert/2-Buy_ticket.jpeg"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
+        <div id="item3" className="carousel-item w-full">
+          <img src="/images/concert/3-Panel.jpeg" className="w-full" />
         </div>
-        {/* <!-- Item 4 --> */}
-        <div
-          className={`${num == 2 ? "" : "hidden "} duration-700 ease-in-out`}
-          data-carousel-item
-        >
-          <img
-            src="images/concert/2-Buy_ticket.jpeg"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
-        </div>
-        {/* <!-- Item 5 --> */}
-        <div
-          className={`${num == 3 ? "" : "hidden "} duration-700 ease-in-out`}
-          data-carousel-item
-        >
-          <img
-            src="images/concert/2-Buy_ticket.jpeg"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
+        <div id="item4" className="carousel-item w-full">
+          <img src="/images/concert/4-Concert.jpeg" className="w-full" />
         </div>
       </div>
-      {/* <!-- Slider indicators --> */}
-      <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-        <button
-          type="button"
-          className="w-3 h-3 rounded-full"
-          aria-current="true"
-          aria-label="Slide 1"
-          data-carousel-slide-to="0"
-        ></button>
-        <button
-          type="button"
-          className="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 2"
-          data-carousel-slide-to="1"
-        ></button>
-        <button
-          type="button"
-          className="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 3"
-          data-carousel-slide-to="2"
-        ></button>
-        <button
-          type="button"
-          className="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 4"
-          data-carousel-slide-to="3"
-        ></button>
-        <button
-          type="button"
-          className="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 5"
-          data-carousel-slide-to="4"
-        ></button>
+      <div className="flex w-full justify-center gap-2 py-2">
+        <a href="#item1" className="btn btn-xs">
+          1
+        </a>
+        <a href="#item2" className="btn btn-xs">
+          2
+        </a>
+        <a href="#item3" className="btn btn-xs">
+          3
+        </a>
+        <a href="#item4" className="btn btn-xs">
+          4
+        </a>
       </div>
-      {/* <!-- Slider controls --> */}
-      <button
-        type="button"
-        onClick={prevFn}
-        className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-hidden"
-        data-carousel-prev
-      >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-hidden">
-          <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M5 1 1 5l4 4"
-            />
-          </svg>
-          <span className="sr-only">Previous</span>
-        </span>
-      </button>
-      <button
-        onClick={nextFn}
-        type="button"
-        className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-hidden"
-        data-carousel-next
-      >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-hidden">
-          <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m1 9 4-4-4-4"
-            />
-          </svg>
-          <span className="sr-only">Next</span>
-        </span>
-      </button>
     </div>
   );
 }
