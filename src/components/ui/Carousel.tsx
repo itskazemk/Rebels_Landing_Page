f1e63ae6d602649ca5e900a3fd2256864f618093
Kazem <itskazemk@gmail.com>
2025-04-20 14:31:37 +0330

    bug fix

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -1,18 +1,24 @@
 export function Carousel() {
   return (
-    <div className="px-8">
-      <div className="carousel w-full">
-        <div id="item1" className="carousel-item w-full">
-          <img src="/images/concert/1-Home_page.jpeg" className="w-full" />
+    <div className="px-8 ">
+      <div className="carousel w-full rounded-2xl">
+        <div id="item1" className="carousel-item w-full  h-256">
+          <img
+            src="/images/concert/1-Home_page.jpeg"
+            className="w-full  h-256"
+          />
         </div>
-        <div id="item2" className="carousel-item w-full">
-          <img src="/images/concert/2-Buy_ticket.jpeg" className="w-full" />
+        <div id="item2" className="carousel-item w-full  h-256">
+          <img
+            src="/images/concert/2-Buy_ticket.jpeg"
+            className="w-full  h-256"
+          />
         </div>
-        <div id="item3" className="carousel-item w-full">
-          <img src="/images/concert/3-Panel.jpeg" className="w-full" />
+        <div id="item3" className="carousel-item w-full  h-256">
+          <img src="/images/concert/3-Panel.jpeg" className="w-full  h-256" />
         </div>
-        <div id="item4" className="carousel-item w-full">
-          <img src="/images/concert/4-Concert.jpeg" className="w-full" />
+        <div id="item4" className="carousel-item w-full  h-256">
+          <img src="/images/concert/4-Concert.jpeg" className="w-full  h-256" />
         </div>
       </div>
       <div className="flex w-full justify-center gap-2 py-2">
